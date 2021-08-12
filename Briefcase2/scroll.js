562d2806f6521f0ba5e10574d1b7f3e993b82895
Kevin Rodas Cuartas <kevin.rodas.cuartas@gmail.com>
2021-08-11 20:31:42 -0500

    Agregamos funcionalidades con .js

--- Briefcase2/scroll.js
+++ Briefcase2/scroll.js
@@ -1,1 +1,10 @@
-alert('si funciona');
+const header = document.getElementById("header-main");
+
+function scrollHeaderTop(){
+    if(window.scrollY > 500) {
+        header.classList.add("fixed-header");
+    }
+}
+
+header.addEventListener("click", scrollHeaderTop);
+document.addEventListener("scroll", scrollHeaderTop);
